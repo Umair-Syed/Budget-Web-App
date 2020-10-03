a7711bc9a0d414b6549f50147a4c1a7b83a91074
Umair Syed <syedumairandrabi66@gmail.com>
2020-10-03 08:31:46 +0530

    Initial Commit

---
+++ app.js
@@ -0,0 +1,24 @@
+//Data Module
+var budgetController = (function() {
+
+   
+
+})();
+
+
+// UI Module.....Handles UI. Example methods like getInputValues() or UpdateUI()
+var UIController = (function() {
+
+    //todo: some code
+
+})();
+
+
+//Controller Module
+//Application Controller, for communication between Data Module (BudgetController) and UI Module (UIController)
+//It will contain methods like event Handlers
+var controller = (function(budgetCtrl, UICtrl) {
+
+    
+
+})(budgetController, UIController);
